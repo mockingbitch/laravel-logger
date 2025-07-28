036c87373035e382d7f13a03440505921cb1ddf1
PhongTran <phong.tran@tabtab.me>
2025-07-28 10:30:34 +0700

    feat: update 2.1 version

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use phongtran\Logger\App\Http\Controllers\LoggerController;
+use phongtran\Logger\app\Http\Controllers\LoggerController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('logger')->group(function () {
+Route::prefix('logger')->middleware(['web'])->group(function () {
     Route::get('/', [LoggerController::class, 'index'])->name('log.index');
     Route::get('/{id}', [LoggerController::class, 'detail'])->name('log.detail');
 });
